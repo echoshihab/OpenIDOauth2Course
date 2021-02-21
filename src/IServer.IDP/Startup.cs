4d3ad03c433a0ad984dd902ac2ce29d68517b8b0
echoshihab <echoshihab@gmail.com>
2021-02-20 19:29:39 -0500

    Persist configuration data

--- src/IServer.IDP/Startup.cs
+++ src/IServer.IDP/Startup.cs
@@ -2,13 +2,18 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 
 namespace IServer.IDP
@@ -24,30 +29,46 @@ namespace IServer.IDP
 
         public void ConfigureServices(IServiceCollection services)
         {
+           
+            var iserverIDPDataDBConnectionString =
+                "Server=(localdb)\\mssqllocaldb;Database=IServerIDPDataDB;Trusted_Connection=True;";
+
+
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
             var builder = services.AddIdentityServer()
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiResources(Config.ApiResources)
-                .AddInMemoryClients(Config.Clients)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiResources(Config.ApiResources)
+                //.AddInMemoryClients(Config.Clients)
                 .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             //builder.AddDeveloperSigningCredential();
             builder.AddSigningCredential(LoadCertificateFromStore());
-        }
 
+            var migrationAssembly = typeof(Startup)
+                                            .GetTypeInfo().Assembly.GetName().Name;
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                            builder.UseSqlServer(iserverIDPDataDBConnectionString, 
+                            options => options.MigrationsAssembly(migrationAssembly));
+            });
+        }
+   
         public void Configure(IApplicationBuilder app)
         {
-           
+
             if (Environment.IsDevelopment())
             {
-                
+            
                 app.UseDeveloperExceptionPage();
             }
 
+            InitializeDatabase(app);
             // uncomment if you want to add MVC
             app.UseStaticFiles();
             app.UseRouting();
@@ -79,5 +100,53 @@ namespace IServer.IDP
 
             }
         }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.
+                GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider
+                        .GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if(!context.Clients.Any())
+                {
+                    foreach(var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if(!context.IdentityResources.Any())
+                {
+                    foreach(var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.ApiResources)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if(!context.ApiScopes.Any())
+                {
+                    foreach(var scope in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(scope.ToEntity());
+
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
     }
 }
--- src/ImageGallery.API/Startup.cs
+++ src/ImageGallery.API/Startup.cs
@@ -28,7 +28,7 @@ namespace ImageGallery.API
         
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {          
             services.AddControllers()
                      .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
             services.AddHttpContextAccessor();
@@ -73,8 +73,10 @@ namespace ImageGallery.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+     
             if (env.IsDevelopment())
             {
+           
                 app.UseDeveloperExceptionPage();
             }
             else
