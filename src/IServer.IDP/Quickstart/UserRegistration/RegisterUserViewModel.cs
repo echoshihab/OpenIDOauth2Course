b08e2d13ebceb80807089a39e6070af5be9d5728
echoshihab <echoshihab@gmail.com>
2021-04-23 20:13:57 -0400

    add user regisration

---
+++ src/IServer.IDP/Quickstart/UserRegistration/RegisterUserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace IServer.IDP.UserRegistration
+{
+    public class RegisterUserViewModel
+    {
+        [MaxLength(200)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Given Name")]
+        public string GivenName { get; set; }
+
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Faimily Name")]
+        public string FamilyName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        public SelectList CountryCodes { get; set; } = 
+            new SelectList
+            (
+                new[]
+                {
+                    new {Id ="BE", Value = "Belgium"},
+                    new {Id = "US", Value="United States of America"},
+                    new {Id = "IN", Value = "India"} 
+                },
+                "Id",
+                "Value");
+
+        public string ReturnUrl { get; set; }
+
+
+    }
+}
---
+++ src/IServer.IDP/Quickstart/UserRegistration/UserRegistrationController.cs
@@ -0,0 +1,95 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Services;
+using IServer.IDP.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IServer.IDP.UserRegistration
+{
+    public class UserRegistrationController: Controller
+    {
+        private readonly ILocalUserService _localUserService;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public UserRegistrationController(ILocalUserService localUserService,
+            IIdentityServerInteractionService interaction)
+        {
+            _localUserService = localUserService ??
+                throw new ArgumentNullException(nameof(localUserService));
+            _interaction = interaction ??
+                throw new ArgumentNullException(nameof(interaction));
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser(string returnUrl)
+        {
+            var vm = new RegisterUserViewModel()
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userToCreate = new Entities.User
+            {
+                Password = model.Password,
+                UserName = model.UserName,
+                Subject = Guid.NewGuid().ToString(),
+                Active = true
+            };
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type= "country",
+                Value = model.Country
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.Address,
+                Value = model.Address
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.GivenName,
+                Value = model.GivenName
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.FamilyName,
+                Value = model.FamilyName
+            });
+
+            _localUserService.AddUser(userToCreate);
+            await _localUserService.SaveChangesAsync();
+
+            //log the user in
+            await HttpContext.SignInAsync(new IdentityServerUser(userToCreate.UserName));
+
+            if (_interaction.IsValidReturnUrl(model.ReturnUrl) 
+                || Url.IsLocalUrl(model.ReturnUrl))
+            {
+                return Redirect(model.ReturnUrl);
+            }
+
+            return Redirect("~/");
+        }
+    }
+}
